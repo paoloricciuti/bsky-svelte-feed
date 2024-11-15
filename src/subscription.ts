50943eb08c8cc2f314ff520ab70c830e67369bd6
paoloricciuti <ricciutipaolo@gmail.com>
2024-11-15 18:50:04 +0100

    fix: ai confidence

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,62 +1,77 @@
-import { inArray } from 'drizzle-orm'
-import { check } from './ai.js'
-import { post } from './db/schema.js'
+import { inArray } from 'drizzle-orm';
+import { check } from './ai.js';
+import { post } from './db/schema.js';
 import {
-  OutputSchema as RepoEvent,
-  isCommit,
-} from './lexicon/types/com/atproto/sync/subscribeRepos.js'
-import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription.js'
+	OutputSchema as RepoEvent,
+	isCommit,
+} from './lexicon/types/com/atproto/sync/subscribeRepos.js';
+import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription.js';
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
-  async handleEvent(evt: RepoEvent) {
-    if (!isCommit(evt)) return
+	async handleEvent(evt: RepoEvent) {
+		if (!isCommit(evt)) return;
 
-    const ops = await getOpsByType(evt)
+		const ops = await getOpsByType(evt);
 
-    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
-    const postsToCreatePromises = await Promise.allSettled(
-      ops.posts.creates
-        .filter((create) => {
-          if (create.record.text.toLowerCase().includes('svelte')) {
-            console.log(create.record.text)
-          }
-          // only svelte-related posts
-          return create.record.text.toLowerCase().includes('svelte')
-        })
-        .map(async (create) => {
-          const confidence = await check(
-            create.record.text +
-              ((await create.get_author()).description ?? ''),
-          )
-          console.log(confidence, create.record.text)
+		const postsToDelete = ops.posts.deletes.map((del) => del.uri);
+		const postsToCreatePromises = await Promise.allSettled(
+			ops.posts.creates
+				.filter((create) => {
+					if (create.record.text.toLowerCase().includes('svelte')) {
+						console.log(create.record.text);
+					}
+					// only svelte-related posts
+					return create.record.text.toLowerCase().includes('svelte');
+				})
+				.map(async (create) => {
+					const text = create.record.text.toLowerCase();
+					let confidence = 0;
+					// if we have this in the sentence we are 100% sure they are talking about
+					// the framework
+					if (
+						text.includes('sveltekit') ||
+						text.includes('svelte-kit') ||
+						text.includes('svelte kit') ||
+						text.includes('sveltejs')
+					) {
+						confidence = 1;
+					} else {
+						const author = await create.get_author();
+						const text_and_bio =
+							create.record.text +
+							' ' +
+							(author.description ? `<bio>${author.description}</bio>` : '');
+						confidence = await check(text_and_bio);
+					}
+					console.log(confidence, create.record.text);
 
-          // map svelte-related posts to a db row
-          return {
-            uri: create.uri,
-            cid: create.cid,
-            indexedAt: new Date().toISOString(),
-            confidence,
-          }
-        }),
-    )
+					// map svelte-related posts to a db row
+					return {
+						uri: create.uri,
+						cid: create.cid,
+						indexedAt: new Date().toISOString(),
+						confidence,
+					};
+				}),
+		);
 
-    const postsToCreate = postsToCreatePromises
-      .filter((post) => post.status === 'fulfilled')
-      .filter((post) => post.value.confidence > 0.8)
-      .map((post) => post.value)
+		const postsToCreate = postsToCreatePromises
+			.filter((post) => post.status === 'fulfilled')
+			.filter((post) => post.value.confidence > 0.8)
+			.map((post) => post.value);
 
-    if (postsToDelete.length > 0) {
-      await this.db
-        .delete(post)
-        .where(inArray(post.uri, postsToDelete))
-        .execute()
-    }
-    if (postsToCreate.length > 0) {
-      await this.db
-        .insert(post)
-        .values(postsToCreate)
-        .onConflictDoNothing()
-        .execute()
-    }
-  }
+		if (postsToDelete.length > 0) {
+			await this.db
+				.delete(post)
+				.where(inArray(post.uri, postsToDelete))
+				.execute();
+		}
+		if (postsToCreate.length > 0) {
+			await this.db
+				.insert(post)
+				.values(postsToCreate)
+				.onConflictDoNothing()
+				.execute();
+		}
+	}
 }
---
+++ src/test-ai.ts
@@ -0,0 +1,27 @@
+import { check } from './ai.js';
+
+console.log(
+	await check(
+		`svelte is the best thing after angularjs
+		<bio>S1 | Developer | sveltelab.dev |  Fat guy | Svelte Ambassador 🔶 | Svelte maintainer 🧡</bio>`,
+	),
+);
+
+console.log(await check('svelte is the best thing after angularjs'));
+console.log(
+	await check(`Saw 
+
+(but didn’t saw my own svelte-related post 😢😅 <bio>Tea & beer drinker, dad of 2 △ web(UI, dev, engineering), dataviz, AI, biology (proteins/antibodies)+ slackline, photo, music, etc. △
+
+Formerly: x.com/0gust1
+
+More personal and up-to-date:
+mastodon instance: merveilles.town/@0gust1</bio>`),
+);
+
+console.log(await check('this cat looks so svelte <bio>i love cats</bio>'));
+console.log(
+	await check(
+		"There's the brick, sitting under the tape machine. For such a svelte machine, it's absolutely huge. And heavy. And sharp. The sad little sticker on the machine itself sums up Newbrain ownership to me ... <bio>Also known as @herebedragons3 on the other site.</bio>",
+	),
+);
