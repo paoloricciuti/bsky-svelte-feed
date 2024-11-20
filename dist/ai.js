608f64755cf02de3f4ce14cd2ac5be3b1e6237d8
paoloricciuti <ricciutipaolo@gmail.com>
2024-11-20 18:24:47 +0100

    feat: better prompt...thanks @khromov

--- dist/ai.js
+++ dist/ai.js
@@ -15,14 +15,52 @@ export async function check(message) {
             model: 'claude-3-5-haiku-20241022',
             max_tokens: 1000,
             temperature: 0,
-            system: 'You need to determine if a tweet is about svelte the web framework. Answer with a single 0 (in case it is not) or 1 in case it is.',
+            system: `Classify whether the following tweet is about the web framework Svelte or its metaframework SvelteKit. 
+
+<examples>
+Tweet: oh my god i love svelte
+Answer: yes
+
+Tweet: my cat is so slick and svelte
+Answer: no
+
+Tweet: svelte just reached 80k stars on github
+Answer: yes
+
+Tweet: brousse is portrayed as a person who is, svelte and thin with strong prescription glasses.
+Answer: no
+
+Tweet: actually, all three band members are usually quite svelte.
+Answer: no
+
+Tweet: after arriving at the lodge, the group is greeted by a svelte, attractive blonde women.
+Answer: no
+
+Tweet: this character is younger and more svelte than the original version and appears to possess wind-based powers.
+Answer: no
+
+Tweet: i've decided to learn svelte
+Answer: yes
+
+Tweet: just found out about svelte...it's soo cool
+Answer: yes
+
+Tweet: i used to like svelte but i'm not sure about the new version
+Answer: yes
+
+Tweet: look at what i did with svelte
+Answer: yes
+</examples>
+
+When the user asks they will write single tweet and your answer needs to either \`yes\` or \`no\``,
             messages: [
                 {
                     role: 'user',
                     content: [
                         {
                             type: 'text',
-                            text: message,
+                            text: `Tweet: ${message}
+Answer: `,
                         },
                     ],
                 },
