ab0720ff93cee459d7d3180986b51cbc6ffa4fbd
paoloricciuti <ricciutipaolo@gmail.com>
2024-11-22 20:16:57 +0100

    chore: try to get more info

--- dist/methods/report-post.js
+++ dist/methods/report-post.js
@@ -1,8 +1,16 @@
 export default function (server, ctx) {
-    server.com.atproto.moderation.createReport(async ({ params, input, auth }) => {
-        console.log(params, input, auth);
+    server.com.atproto.moderation.createReport(async ({ input, req }) => {
+        console.log(input, req.body);
         return {
             status: 200,
+            encoding: 'application/json',
+            body: {
+                createdAt: new Date().toISOString(),
+                id: 0,
+                reasonType: 'com.atproto.moderation.defs#reasonOther',
+                reportedBy: '',
+                subject: { ...input },
+            },
         };
     });
 }
--- src/methods/report-post.ts
+++ src/methods/report-post.ts
@@ -2,12 +2,18 @@ import { AppContext } from '../config.js';
 import { Server } from '../lexicon/index.js';
 
 export default function (server: Server, ctx: AppContext) {
-	server.com.atproto.moderation.createReport(
-		async ({ params, input, auth }) => {
-			console.log(params, input, auth);
-			return {
-				status: 200,
-			};
-		},
-	);
+	server.com.atproto.moderation.createReport(async ({ input, req }) => {
+		console.log(input, req.body);
+		return {
+			status: 200,
+			encoding: 'application/json',
+			body: {
+				createdAt: new Date().toISOString(),
+				id: 0,
+				reasonType: 'com.atproto.moderation.defs#reasonOther',
+				reportedBy: '',
+				subject: { ...input },
+			},
+		};
+	});
 }
