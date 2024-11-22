39deaf683785f44f7253a5fcd299190d963960e1
paoloricciuti <ricciutipaolo@gmail.com>
2024-11-22 20:42:59 +0100

    feat: actually add or remove posts

--- src/methods/report-post.ts
+++ src/methods/report-post.ts
@@ -1,22 +1,56 @@
+import { eq } from 'drizzle-orm';
 import { validateAuth } from '../auth.js';
 import { AppContext } from '../config.js';
+import { post } from '../db/schema.js';
 import { Server } from '../lexicon/index.js';
 
 export default function (server: Server, ctx: AppContext) {
 	server.com.atproto.moderation.createReport(async ({ input, req }) => {
 		const requesterDid = await validateAuth(req, null, ctx.didResolver);
 
-		console.log(input, req.rawHeaders, requesterDid);
+		if (requesterDid !== process.env.FEEDGEN_PUBLISHER_DID) {
+			return {
+				status: 401,
+			};
+		}
+
+		if (
+			typeof input.body.subject.uri !== 'string' ||
+			typeof input.body.subject.cid !== 'string'
+		) {
+			return {
+				status: 500,
+			};
+		}
+
+		const [already_exists] = await ctx.db
+			.select()
+			.from(post)
+			.where(eq(post.uri, input.body.subject.uri))
+			.execute();
+
+		if (already_exists) {
+			await ctx.db
+				.delete(post)
+				.where(eq(post.uri, already_exists.uri!))
+				.execute();
+		} else {
+			await ctx.db
+				.insert(post)
+				.values({
+					cid: input.body.subject.cid,
+					uri: input.body.subject.uri,
+					indexedAt: new Date().toISOString(),
+				})
+				.onConflictDoNothing()
+				.execute();
+		}
 
 		return {
 			status: 200,
 			encoding: 'application/json',
 			body: {
 				createdAt: new Date().toISOString(),
-				id: 0,
-				reasonType: 'com.atproto.moderation.defs#reasonOther',
-				reportedBy: '',
-				subject: { ...input },
 			},
 		};
 	});
