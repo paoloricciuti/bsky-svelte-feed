96779189a6b4a6424832b7372cb1484048047d32
paoloricciuti <ricciutipaolo@gmail.com>
2024-12-13 12:07:18 +0100

    feat: add ability to approve and delete post from confirm page

--- src/confirm-posts.ts
+++ src/confirm-posts.ts
@@ -2,11 +2,17 @@ import { and, desc, eq, notLike } from 'drizzle-orm';
 import express from 'express';
 import { AppContext } from './config.js';
 import { post } from './db/schema.js';
+import { Agent } from '@atproto/api';
+import cookie from 'cookie-parser';
 
 const makeRouter = (ctx: AppContext) => {
 	const router = express.Router();
 
+	router.use(cookie());
+
 	router.get('/confirm', async (_req, res) => {
+		const agent = new Agent('https://public.api.bsky.app');
+
 		let results = await ctx.db
 			.select()
 			.from(post)
@@ -20,16 +26,125 @@ const makeRouter = (ctx: AppContext) => {
 			.execute();
 		res.setHeader('Content-type', 'text/html');
 
-		return res.send(
-			results
-				.map(
-					(result) =>
-						`<a target="_blank" href="${result.uri
-							?.replace('at://', 'https://bsky.app/profile/')
-							.replace('app.bsky.feed.post', 'post')}">${result.uri}</a><br/>`,
-				)
-				.join('\n'),
+		const posts = await Promise.allSettled(
+			results.map(async (result) => {
+				const post = await agent.getPostThread({
+					uri: result.uri!,
+				});
+
+				return `
+				<div class="card">
+				${(post.data.thread.post as any).record.text}
+				<div class="actions">
+					<a target="_blank" href="${result.uri
+						?.replace('at://', 'https://bsky.app/profile/')
+						.replace('app.bsky.feed.post', 'post')}">Open on bsky</a>
+					<a target="_blank" href="/confirm/approve?id=${result.uri}">Approve</a>
+					<a target="_blank" href="/confirm/delete?id=${result.uri}">Delete</a>
+				</div>
+				</div>`;
+			}),
 		);
+
+		let html = `
+		<style>
+			main{
+				display: grid;
+				grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+				gap: 1rem;
+			}
+			.card {
+				display: grid;
+				place-items: center;
+				font-family: sans-serif;
+				align-content: space-between;
+				gap: 1rem;
+				background-color: #ff3e0033;
+				border-radius: .5rem;
+				padding: 1rem;
+			}
+			.card a{
+				display: block;
+				background-color: #ff3e00;
+				border-radius: .5rem;
+				padding: .5rem;
+				color: white;
+				font-weight: bold;
+				text-decoration: none;
+				width: 100%;
+				text-align: center;
+			}
+			.actions{
+				display: flex;
+				width: 100%;
+				gap: .5rem;
+			}
+		</style>
+		<main>
+		${posts
+			.filter((res) => res.status === 'fulfilled')
+			.map((res) => res.value)
+			.join('\n')}
+		</main>`;
+		return res.send(html);
+	});
+
+	router.get('/confirm/approve', async (req, res) => {
+		const id = req.query.id;
+
+		if (!id || req.cookies['bsky-feed-pass'] !== process.env.APPROVE_PASSWORD) {
+			res.setHeader('Content-type', 'text/html');
+			let html = `
+			<h1 class="font-familiy: sans-serif">Unauth!</h1>
+			<script>
+			setTimeout(()=>window.close(), 5000);
+			</script>
+			`;
+			return res.send(html);
+		}
+
+		await ctx.db
+			.update(post)
+			.set({
+				confirmed: true,
+			})
+			.where(eq(post.uri, id.toString()))
+			.execute();
+
+		res.setHeader('Content-type', 'text/html');
+		let html = `
+		<h1 class="font-familiy: sans-serif">Done!</h1>
+		<script>
+			setTimeout(()=>window.close(), 5000);
+		</script>
+		`;
+		return res.send(html);
+	});
+
+	router.get('/confirm/delete', async (req, res) => {
+		const id = req.query.id;
+
+		if (!id || req.cookies['bsky-feed-pass'] !== process.env.APPROVE_PASSWORD) {
+			res.setHeader('Content-type', 'text/html');
+			let html = `
+			<h1 class="font-familiy: sans-serif">Unauth!</h1>
+			<script>
+			setTimeout(()=>window.close(), 5000);
+			</script>
+			`;
+			return res.send(html);
+		}
+
+		await ctx.db.delete(post).where(eq(post.uri, id.toString())).execute();
+
+		res.setHeader('Content-type', 'text/html');
+		let html = `
+		<h1 class="font-familiy: sans-serif">Done!</h1>
+		<script>
+			setTimeout(()=>window.close(), 5000);
+		</script>
+		`;
+		return res.send(html);
 	});
 
 	return router;
