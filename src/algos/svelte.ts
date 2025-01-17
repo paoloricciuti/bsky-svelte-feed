ed3511dd27d5a5e5d72f38cf4fb33cd2ea0ab864
paoloricciuti <ricciutipaolo@gmail.com>
2025-01-17 10:45:58 +0100

    fix: duh

--- src/algos/svelte.ts
+++ src/algos/svelte.ts
@@ -33,11 +33,9 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
 		post: row.uri!,
 	}));
 
-	console.log('limit & length', feed.length, params.limit);
-
 	if (
 		Math.random() > 0.9 &&
-		feed.findIndex((post) => post.post !== LABELER_POST_URI) === -1
+		feed.findIndex((post) => post.post === LABELER_POST_URI) === -1
 	) {
 		console.log('Pushing labeler post');
 		feed.unshift({
